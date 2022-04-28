695741f0c4275b8dff014a4aa80273fb4396241c
israel2x <ezurita@fiec.espol.edu.ec>
2022-04-28 18:20:49 -0500

    Congiguración de logs con el paquete winston

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const winston = require("winston");
 const app = express();
 
+//require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/prod")(app);
 require("./startup/config")();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}.... `));
+//app.listen(port, () => console.log(`Listening on port ${port}.... `));
+const server = app.listen(port, () =>
+  winston.info(`Listening on port ${port}...`)
+);
+
+module.exports = server;
--- startup/routes.js
+++ startup/routes.js
@@ -1,10 +1,9 @@
-const express = require('express');
-const notes = require('../routes/notes');
+const express = require("express");
+const error = require("../middleware/error");
+const notes = require("../routes/notes");
 
-
-module.exports = function(app) {
-    
-    app.use(express.json());
-    app.use('/api/notes', notes);
-
-}
+module.exports = function (app) {
+  app.use(express.json());
+  app.use("/api/notes", notes);
+  app.use(error);
+};
