bfd2fb59d46e5ebbbcbba042ba89bf79ba6230bf
israel2x <ezurita@fiec.espol.edu.ec>
2022-04-28 12:31:13 -0500

    Configuracion básica

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-require('./startup/routes')(app);
-require('./startup/db')();
-
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/prod")(app);
+require("./startup/config")();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}.... `));
-
--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const config = require("config");
 
-module.exports = function() {
+module.exports = function () {
+  const db = config.get("db");
 
-    mongoose.connect('mongodb://localhost/notes')
-        .then(() => console.log('Connected to MongoDB...'))
-        .catch(err => console.error('Could not connect to MongoDB...'));
-        
-}
+  mongoose
+    .connect(db)
+    .then(() => console.log("Connected to MongoDB..."))
+    .catch((err) => console.error("Could not connect to MongoDB..."));
+};
