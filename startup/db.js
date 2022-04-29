b498a2be8b5ff153de6d9432c658512dccccaf2c
israel2x <ezurita@fiec.espol.edu.ec>
2022-04-29 01:18:51 -0500

    Correcion archivo config parametro db

--- startup/db.js
+++ startup/db.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 const config = require("config");
 
 module.exports = function () {
-  const db = config.get("db");
+  const dbm = config.get("db");
 
-  console.log(db);
+  console.log(dbm);
 
   mongoose
-    .connect(db)
+    .connect(dbm)
     .then(() => console.log("Connected to MongoDB..."))
     .catch((err) => console.error("Could not connect to MongoDB..."));
 };
