a1f9e49cef3d93b35ce9af426c41a9d5bfb0f30e
israel2x <ezurita@fiec.espol.edu.ec>
2022-05-03 20:31:51 -0500

    Correcion del header en la peticion http del api/notes/

--- routes/notes.js
+++ routes/notes.js
@@ -1,57 +1,68 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { Note, validate } = require('../models/note');
-
+const { Note, validate } = require("../models/note");
 
 //get all notes
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
+  try {
     const notes = await Note.find();
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.send(notes);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ type: "error", message: error.message });
+  }
 });
 
 //new note
-router.post('/', async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    let note = new Note({
-        text : req.body.text,
-        category: [ ...req.body.category ],
-        favority: req.body.favority,
-        scratchpad: req.body.scratchpad,
-        created: req.body.created,
-        lastUpdated: req.body.lastUpdated
-    });
+  let note = new Note({
+    text: req.body.text,
+    category: [...req.body.category],
+    favority: req.body.favority,
+    scratchpad: req.body.scratchpad,
+    created: req.body.created,
+    lastUpdated: req.body.lastUpdated,
+  });
 
-    note = await note.save();
-    res.send(note);
+  note = await note.save();
+  res.send(note);
 });
 
 //update note
-router.put('/:id', async (req, res) => {
-    const { error } = validate(req.body); 
-    if (error) return res.status(400).send(error.details[0].message);
-    
-    const note = await Note.findByIdAndUpdate(req.params.id, {
-        text : req.body.text,
-        category: [ ...req.body.category ],
-        favority: req.body.favority,
-        scratchpad: req.body.scratchpad,
-        created: req.body.created,
-        lastUpdated: req.body.lastUpdated
-    }, { new: true });
+router.put("/:id", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    if (!note) return res.status(404).send('The note with the given ID was not found.');
-  
-    res.send(note);
+  const note = await Note.findByIdAndUpdate(
+    req.params.id,
+    {
+      text: req.body.text,
+      category: [...req.body.category],
+      favority: req.body.favority,
+      scratchpad: req.body.scratchpad,
+      created: req.body.created,
+      lastUpdated: req.body.lastUpdated,
+    },
+    { new: true }
+  );
+
+  if (!note)
+    return res.status(404).send("The note with the given ID was not found.");
+
+  res.send(note);
 });
 
-//delete note 
-router.delete('/:id', async(req, res) => {
-    const note = await Note.findByIdAndRemove(req.params.id);
-    if (!note) return res.status(404).send('The note with the given ID was not found.');
+//delete note
+router.delete("/:id", async (req, res) => {
+  const note = await Note.findByIdAndRemove(req.params.id);
+  if (!note)
+    return res.status(404).send("The note with the given ID was not found.");
 
-    res.send(note);
+  res.send(note);
 });
 
 module.exports = router;
