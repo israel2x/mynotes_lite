a07586442f55497120a40bb577b91405f4ec3a26
israel2x <ezurita@fiec.espol.edu.ec>
2022-05-03 19:44:36 -0500

    Configuración CORS en el deploy en vercel

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
 const env = process.env;
 
 const config = {
-  db:
-    env.mynotes_db ||
-    "mongodb+srv://dbnoteslite:boW8WDxf1KCY3Hlw@clusternotes.m6rec.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  db: env.mynotes_db,
   jwtPrivateKey: env.jwtPrivateKey || 1234,
 };
 
